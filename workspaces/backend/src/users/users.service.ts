04586d78dd1782b79cd42486e4c0a63267ed2dec
Mattias Söderberg <mattes.email@gmail.com>
2022-10-23 21:44:29 +0200

    Add logger and type returns

--- workspaces/backend/src/users/users.service.ts
+++ workspaces/backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -12,38 +12,43 @@ export class UsersService {
     @InjectModel(User.name)
     private readonly userModel: Model<UserDocument>,
   ) {}
-  async create(createUserDto: CreateUserDto) {
+
+  private readonly logger = new Logger(UsersService.name);
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
     const hash = await bcrypt.hash(
       createUserDto.password,
       parseInt(process.env.SALT_ROUNDS),
     );
     createUserDto.password = hash;
     const newUser = await this.userModel.create(createUserDto);
+    this.logger.log(`New user created ${newUser.email} ${newUser.id}`);
     return newUser;
   }
 
-  async findAll() {
+  async findAll(): Promise<User[]> {
     const users = await this.userModel.find();
     return users;
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<User> {
     const user = await this.userModel.findById(id);
     return user;
   }
 
-  async findOneByEmail(email: string) {
+  async findOneByEmail(email: string): Promise<User> {
     return await this.userModel.findOne({ email });
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto) {
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
     const user = await this.userModel.findByIdAndUpdate(id, updateUserDto, {
       returnDocument: 'after',
     });
+    this.logger.log(`User: ${user.id} updated ${updateUserDto}`);
     return user;
   }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<User> {
     const user = await this.userModel.findByIdAndDelete(id);
     return user;
   }
