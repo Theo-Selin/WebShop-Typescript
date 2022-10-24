f406f0c810b3cbcbacd533ce431a5ab29bc2d35f
Theo Selin <theo.selin@hotmail.com>
2022-10-24 14:01:58 +0200

    Initial frontend commit. Header mockup

--- workspaces/frontend/components/Header.tsx
+++ workspaces/frontend/components/Header.tsx
@@ -1,32 +1,60 @@
+import Image from "next/image";
+import Link from "next/link";
+import WebShopLogo from "../assets/WebShopLogo.png";
 import React from "react";
-import { Box, Image, Button } from "@chakra-ui/react";
+import {
+  SearchIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
+  const token = false;
+
   return (
-    <>
-      <header>
-        <Box
-          display="flex"
-          align-items="center"
-          justifyContent="space-between"
-          ml={6}
-          mr={6}
-        >
-          <Box>
-            <Image boxSize="90px" src="" alt="brand" />
-          </Box>
-          <Box>Navigation options</Box>
-          <Box>
-            <Button pr={3} colorScheme="gray.600" fontSize="sm" variant="link">
-              Login
-            </Button>
-            <Button colorScheme="gray.600" fontSize="sm" variant="link">
-              Sign up
-            </Button>
-          </Box>
-        </Box>
-      </header>
-    </>
+    <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4">
+      <div className="flex items-center justify-center md:w-1/5">
+        <Link href="/">
+          <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+            <Image src={WebShopLogo} layout="fill" objectFit="contain" />
+          </div>
+        </Link>
+      </div>
+      <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+        <a className="headerLink">Product</a>
+        <a className="headerLink">Explore</a>
+        <a className="headerLink">Support</a>
+        <a className="headerLink">Business</a>
+      </div>
+      <div className="flex items-center justify-center space-x-4 md:w-1/5">
+        <SearchIcon className="headerIcon" />
+        <Link href="/checkout">
+          <div className="relative cursor-pointer">
+            <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
+              5
+            </span>
+            <ShoppingBagIcon className="headerIcon" />
+          </div>
+        </Link>
+
+        {/* Change icon depending if the user is logged in or not */}
+        {token ? (
+          <Image
+            src="https://drivma.se/wp-content/uploads/2020/11/blank-profile-picture-973460_640.png"
+            alt=""
+            className="cursor-pointer rounded-full"
+            width={34}
+            height={34}
+            // onClick={() => signOut()}
+          />
+        ) : (
+          <UserIcon
+            className="headerIcon"
+            // onClick={() => signIn()}
+          />
+        )}
+      </div>
+    </header>
   );
 };
 
--- workspaces/frontend/pages/_app.tsx
+++ workspaces/frontend/pages/_app.tsx
@@ -1,13 +1,8 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import theme from "../styles/theme/theme";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
