7493b71bafb8da142ad83ce1aae653e107d761e5
Mattias Söderberg <mattes.email@gmail.com>
2022-10-26 11:25:13 +0200

    Add initProducts on startup

--- workspaces/backend/src/main.ts
+++ workspaces/backend/src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initProducts } from './utils/initProducts';
 
 const port = 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  await initProducts();
   await app.listen(port);
   console.log(`server is listening on port: ${port}`);
 }
