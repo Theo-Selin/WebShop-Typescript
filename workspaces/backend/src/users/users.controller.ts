7927c7be5191770638c03eafcf49a38cc34f5210
Mattias Söderberg <mattes.email@gmail.com>
2022-10-29 13:59:21 +0200

    Add /me get route

--- workspaces/backend/src/users/users.controller.ts
+++ workspaces/backend/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   UseInterceptors,
+  Req,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -18,6 +19,13 @@ import { Role } from '../auth/role.enum';
 import { Public } from '../auth/decorators/public.decorator';
 import { ResponseDto } from '../utils/response-dto.decorator';
 import { FindOneResponseDto } from './dto/find-one-response.dto';
+import { Request } from 'express';
+
+interface JwtPayload {
+  userId: string;
+  email: string;
+  role: string;
+}
 
 @UseInterceptors(MongooseClassSerializerInterceptor({ defaultClass: User }))
 @Controller('users')
@@ -34,6 +42,13 @@ export class UsersController {
   @Get()
   async findAll() {
     return await this.usersService.findAll();
+  }
+
+  @ResponseDto(FindOneResponseDto)
+  @Get('me')
+  async findLoggedInUser(@Req() req: Request) {
+    const { userId } = req.user as JwtPayload;
+    return await this.usersService.findOne(userId.toString());
   }
 
   @ResponseDto(FindOneResponseDto)
