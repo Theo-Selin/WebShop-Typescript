eaa114965c12793474b6fccf51fc4c984c7c0169
Mattias Söderberg <mattes.email@gmail.com>
2022-10-30 19:31:50 +0100

    Add name to Logger

--- workspaces/backend/src/utils/exceptionsLogger.filter.ts
+++ workspaces/backend/src/utils/exceptionsLogger.filter.ts
@@ -3,7 +3,7 @@ import { BaseExceptionFilter } from '@nestjs/core';
 
 @Catch()
 export class ExceptionsLoggerFilter extends BaseExceptionFilter {
-  private readonly logger = new Logger();
+  private readonly logger = new Logger('ExceptionsLogger');
   catch(exception: unknown, host: ArgumentsHost) {
     this.logger.error(exception);
     super.catch(exception, host);
