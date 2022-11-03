fea7d8ffcfeb0c9fa6747c010f4f43e72952fd73
imakero <45946863+imakero@users.noreply.github.com>
2022-11-03 12:56:44 +0100

    fix: require productId in updateQuantityDto

--- workspaces/backend/src/carts/dto/update-quantity-cart.dto.ts
+++ workspaces/backend/src/carts/dto/update-quantity-cart.dto.ts
@@ -1,6 +1,8 @@
-import { IsInt, Min } from 'class-validator';
+import { IsInt, IsMongoId, IsNotEmpty, Min } from 'class-validator';
 
 export class UpdateQuantityDto {
+  @IsNotEmpty()
+  @IsMongoId()
   productId: string;
   @Min(0, { message: '$property must be greater than 0' })
   @IsInt({ message: '$property must be an integer' })
