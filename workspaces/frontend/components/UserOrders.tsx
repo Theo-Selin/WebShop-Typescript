134bace970166a7624eeea30f8bca9208532b51a
imakero <45946863+imakero@users.noreply.github.com>
2022-11-09 16:10:53 +0100

    update user orders page for new order component

--- workspaces/frontend/components/UserOrders.tsx
+++ workspaces/frontend/components/UserOrders.tsx
@@ -8,9 +8,10 @@ const UserOrders = ({ orders }: Props) => {
   return (
     <div className="mx-auto my-24 max-w-5xl pb-24">
       <div className="mx-5 md:mx-8">
+        <h1 className="text-2xl font-semibold">Order history</h1>
         <div className="my-12 mt-6 ml-auto w-full">
           {orders.map((order) => (
-            <Order order={order} key={order._id} />
+            <Order order={order} isAdmin={false} key={order._id} />
           ))}
         </div>
       </div>
