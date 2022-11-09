ee7dd5177bf9dbc53d5fe92394e62e4eb6944a34
Theo Selin <theo.selin@hotmail.com>
2022-11-09 11:28:19 +0100

    Hardcoded thumbnail gallery for details

--- workspaces/frontend/components/ImageGallery.tsx
+++ workspaces/frontend/components/ImageGallery.tsx
@@ -1,57 +1,27 @@
 import React from "react";
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from "react-image-gallery";
 
 interface ProductProps {
   product: Product;
 }
 
-const ImageGallery = ({ product }: ProductProps) => {
-  return (
-    <div>
-      {product.images.length ? (
-        <section className="overflow-hidden text-gray-700">
-          <div className="mx-auto flex max-w-lg flex-wrap">
-            <div className="w-full p-1 md:p-2">
-              <img
-                alt="gallery"
-                className="mx-auto block h-full rounded-lg object-contain object-center"
-                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[0].path}`}
-              />
-            </div>
-
-            {product.images.length > 1 && (
-              <div className="w-1/4 p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-contain object-center"
-                  src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[1].path}`}
-                />
-              </div>
-            )}
-
-            {product.images.length > 2 && (
-              <div className="w-1/2 p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[2].path}`}
-                />
-              </div>
-            )}
-
-            {product.images.length > 3 && (
-              <div className="w-1/2 p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[3].path}`}
-                />
-              </div>
-            )}
-          </div>
-        </section>
-      ) : null}
-    </div>
-  );
+const ImageGalleryComponent = ({ product }: ProductProps) => {
+  const images = [
+    {
+      original: `${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[0].path}`,
+      thumbnail: `${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[0].path}`,
+    },
+    {
+      original: `${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[1].path}`,
+      thumbnail: `${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[1].path}`,
+    },
+    {
+      original: `${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[2].path}`,
+      thumbnail: `${process.env.NEXT_PUBLIC_BACKEND_URL}/${product.images[2].path}`,
+    },
+  ];
+  return images ? <ImageGallery items={images} /> : null;
 };
 
-export default ImageGallery;
+export default ImageGalleryComponent;
