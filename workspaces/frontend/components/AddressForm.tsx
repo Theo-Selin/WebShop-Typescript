b84ff8af5e170b6173dcb8fee2d00b2c6ac36990
imakero <45946863+imakero@users.noreply.github.com>
2022-11-09 11:57:40 +0100

    feature: change address at checkout

--- workspaces/frontend/components/AddressForm.tsx
+++ workspaces/frontend/components/AddressForm.tsx
@@ -1,103 +1,61 @@
 import React from "react";
-import { Formik, FormikHelpers, Form, Field } from "formik";
-import Head from "next/head";
-import Button from "./Button";
-
-interface Values {
-  streetAddress: string;
-  zipCode: string;
-  city: string;
-  country: string;
-}
+import { Form, Field } from "formik";
 
 const AddressForm = () => {
   return (
     <>
-      <Head>
-        <title>Admin - New Product</title>
-        <link rel="icon" href="/WebShopLogo.png"></link>
-      </Head>
-      <Formik
-        initialValues={{
-          streetAddress: "",
-          zipCode: "",
-          city: "",
-          country: "",
-        }}
-        onSubmit={(
-          values: Values,
-          { setSubmitting, resetForm }: FormikHelpers<Values>
-        ) => {
-          const payload = { ...values };
-          // createAddress.mutate(payload, {
-          //   onSuccess: () => {
-          //     toast.success(`Address, ${values.streetAddress}, added!`, {
-          //       position: "top-center",
-          //       className: "text-sm",
-          //     });
-          //     setSubmitting(false);
-          //     resetForm();
-          //   },
-          // });
-        }}
-      >
-        <main className="my-24">
-          <div className="flex items-center justify-center p-12">
-            <div className="mx-auto w-full max-w-[550px] bg-white p-10 shadow-lg ring-1 ring-black ring-opacity-5">
-              <Form className="py-6 px-9">
-                <div className="mb-5">
-                  <label className="mb-3 block text-base font-medium text-[#07074D]">
-                    Street:
-                    <Field
-                      name="streetAddress"
-                      id="streetAddress"
-                      placeholder="where do you live"
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-violet-400 focus:shadow-md"
-                    />
-                  </label>
-                </div>
+      <main className="my-24">
+        <div className="flex items-center justify-center p-12">
+          <div className="mx-auto w-full max-w-[550px] bg-white p-10 shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="py-6 px-9">
+              <div className="mb-5">
+                <label className="mb-3 block text-base font-medium text-[#07074D]">
+                  Street:
+                  <Field
+                    name="streetAddress"
+                    id="streetAddress"
+                    placeholder="where do you live"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-violet-400 focus:shadow-md"
+                  />
+                </label>
+              </div>
 
-                <div className="mb-5">
-                  <label className="mb-3 block text-base font-medium text-[#07074D]">
-                    City:
-                    <Field
-                      name="city"
-                      id="city"
-                      placeholder="where the street lives"
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-violet-400 focus:shadow-md"
-                    />
-                  </label>
-                </div>
+              <div className="mb-5">
+                <label className="mb-3 block text-base font-medium text-[#07074D]">
+                  City:
+                  <Field
+                    name="city"
+                    id="city"
+                    placeholder="where the street lives"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-violet-400 focus:shadow-md"
+                  />
+                </label>
+              </div>
 
-                <div className="mb-5 flex gap-12">
-                  <label className="mb-3 block text-base font-medium text-[#07074D]">
-                    Zip code:
-                    <Field
-                      name="zipCode"
-                      id="zipCode"
-                      placeholder="Postal code"
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-violet-400 focus:shadow-md"
-                    />
-                  </label>
-                  <label className="mb-3 block text-base font-medium text-[#07074D]">
-                    Country
-                    <Field
-                      name="country"
-                      id="country"
-                      placeholder="SWE"
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-violet-400 focus:shadow-md"
-                    />
-                  </label>
-                </div>
-
-                <div>
-                  <Button title="Save" width="w-full" type="submit" />
-                </div>
-              </Form>
+              <div className="mb-5 flex gap-12">
+                <label className="mb-3 block text-base font-medium text-[#07074D]">
+                  Zip code:
+                  <Field
+                    name="zipCode"
+                    id="zipCode"
+                    placeholder="Postal code"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-violet-400 focus:shadow-md"
+                  />
+                </label>
+                <label className="mb-3 block text-base font-medium text-[#07074D]">
+                  Country
+                  <Field
+                    name="country"
+                    id="country"
+                    placeholder="SWE"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-violet-400 focus:shadow-md"
+                  />
+                </label>
+              </div>
             </div>
           </div>
-        </main>
-      </Formik>
+        </div>
+      </main>
     </>
   );
 };
--- workspaces/frontend/pages/checkout.tsx
+++ workspaces/frontend/pages/checkout.tsx
@@ -8,6 +8,15 @@ import BaseLayout from "../components/BaseLayout";
 import useCart from "../utils/hooks/useCart";
 import AddressForm from "../components/AddressForm";
 import PostCheckout from "../components/PostCheckout";
+import { Form, Formik, FormikHelpers } from "formik";
+import { toast } from "react-hot-toast";
+
+interface Values {
+  streetAddress: string;
+  zipCode: string;
+  city: string;
+  country: string;
+}
 
 const Checkout = () => {
   const [isCheckedOut, setIsCheckedOut] = useState(false);
@@ -39,110 +48,145 @@ const Checkout = () => {
     return null;
   }
 
-  const handleCheckout = () => {
-    checkoutOrder.mutate(undefined, {
-      onSuccess: () => {
-        setIsCheckedOut(true);
-      },
-    });
-  };
-
   if (isCheckedOut) {
     return <PostCheckout />;
   }
 
   return (
     <main className="mx-auto max-w-5xl pb-24">
-      <div className="flex flex-col items-center px-5">
-        <h1 className="mt-20 text-3xl font-semibold lg:text-4xl">
-          {cart && cart.products.length > 0
-            ? "Review your cart"
-            : "Your cart is empty"}
-        </h1>
-        <p className="my-4">Free delivery and returns</p>
-        <Button title="Continue shopping" onClick={() => router.push("/")} />
-      </div>
-      {cart && cart.products.length > 0 && (
-        <div className="mx-5 md:mx-8">
-          <CheckoutProduct />
-          <div className="my-12 mt-6 ml-auto w-full">
-            <div className="divide-y divide-gray-300">
-              <div className="pb-4">
-                <div className="flex justify-between">
-                  <p>Subtotal:</p>
-                  <p>{price}:-</p>
-                </div>
-                <div className="flex justify-between">
-                  <p>Weight:</p>
-                  <p>{weight}kg</p>
-                </div>
-                <div className="flex justify-between">
-                  <p>Shipping:</p>
-                  <p>Free</p>
-                </div>
-                <div className="flex justify-between">
-                  <div className="flex flex-col gap-x-1 lg:flex-row">
-                    Estimated tax for:{" "}
-                    <p
-                      onClick={() =>
-                        visible && visible === true
-                          ? setVisible(false)
-                          : setVisible(true)
-                      }
-                      className="flex cursor-pointer items-end text-blue-500 hover:underline"
-                    >
-                      Enter Address
-                      <ChevronDownIcon className="h-6 w-6" />
-                    </p>
-                  </div>
-                  <p>tax amount</p>
-                </div>
-                {visible && visible === true && <AddressForm />}
-              </div>
-              <div className="flex justify-between pt-4 text-xl font-semibold">
-                <h4>Total</h4>
-                <h4>{price}:-</h4>
-              </div>
-            </div>
-            <div className="my-14 space-y-4">
-              <h4 className="text-xl font-semibold">Check out your purchase</h4>
-              <div className="flex flex-col gap-4 md:flex-row">
-                <div className="order-2 flex flex-1 flex-col items-center rounded-xl bg-gray-200 p-8 py-12 text-center">
-                  <h4 className="mb-4 flex flex-col text-xl font-semibold">
-                    <span>Pay Directly</span>
-                    <span>with Swish</span>
-                  </h4>
-                  <Button title="Checkout" onClick={handleCheckout} />
-                  <p className="mt-2 max-w-[300px] text-[12px]">
-                    We do not save any kind of information given to us, whether
-                    it be your address or contact information, unless it&apos;s
-                    valuable information that could generate us more money in
-                    the end
-                  </p>
-                </div>
-
-                <div className="flex flex-1 flex-col items-center space-y-8 rounded-xl bg-gray-200 p-8 py-12 text-center md:order-2">
-                  <h4 className="mb-4 flex flex-col text-xl font-semibold">
-                    <span>Pay Monthly</span>
-                    <span>with Klarna</span>
-                  </h4>
-                  <Button
-                    noIcon
-                    title="Check out with Klarna"
-                    onClick={handleCheckout}
-                  />
-                  <p className="mt-2 max-w-[300px] text-[12px]">
-                    We do not save any kind of information given to us, whether
-                    it be your address or contact information, unless it&apos;s
-                    valuable information that could generate us more money in
-                    the end
-                  </p>
-                </div>
-              </div>
-            </div>
+      <Formik
+        initialValues={{
+          streetAddress: user.deliveryAddress?.streetAddress || "",
+          zipCode: user.deliveryAddress?.zipCode || "",
+          city: user.deliveryAddress?.city || "",
+          country: user.deliveryAddress?.country || "",
+        }}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikHelpers<Values>
+        ) => {
+          checkoutOrder.mutate(
+            { deliveryAddress: { ...values } },
+            {
+              onSuccess: () => {
+                setIsCheckedOut(true);
+                toast.success("Your order has been placed!", {
+                  position: "top-center",
+                  className: "text-sm",
+                });
+                setSubmitting(false);
+              },
+              onError: () => {
+                toast.error(
+                  "Something went wrong, please double-check the address!",
+                  {
+                    position: "top-center",
+                    className: "text-sm",
+                  }
+                );
+              },
+            }
+          );
+        }}
+      >
+        <Form>
+          <div className="flex flex-col items-center px-5">
+            <h1 className="mt-20 text-3xl font-semibold lg:text-4xl">
+              {cart && cart.products.length > 0
+                ? "Review your cart"
+                : "Your cart is empty"}
+            </h1>
+            <p className="my-4">Free delivery and returns</p>
+            <Button
+              title="Continue shopping"
+              onClick={() => router.push("/")}
+            />
           </div>
-        </div>
-      )}
+          {cart && cart.products.length > 0 && (
+            <div className="mx-5 md:mx-8">
+              <CheckoutProduct />
+              <div className="my-12 mt-6 ml-auto w-full">
+                <div className="divide-y divide-gray-300">
+                  <div className="pb-4">
+                    <div className="flex justify-between">
+                      <p>Subtotal:</p>
+                      <p>{price}:-</p>
+                    </div>
+                    <div className="flex justify-between">
+                      <p>Weight:</p>
+                      <p>{weight}kg</p>
+                    </div>
+                    <div className="flex justify-between">
+                      <p>Shipping:</p>
+                      <p>Free</p>
+                    </div>
+                    <div className="flex justify-between">
+                      <div className="flex flex-col gap-x-1 lg:flex-row">
+                        Estimated tax for:{" "}
+                        <p
+                          onClick={() =>
+                            visible && visible === true
+                              ? setVisible(false)
+                              : setVisible(true)
+                          }
+                          className="flex cursor-pointer items-end text-blue-500 hover:underline"
+                        >
+                          Enter Address
+                          <ChevronDownIcon className="h-6 w-6" />
+                        </p>
+                      </div>
+                      <p>tax amount</p>
+                    </div>
+                    {visible && <AddressForm />}
+                  </div>
+                  <div className="flex justify-between pt-4 text-xl font-semibold">
+                    <h4>Total</h4>
+                    <h4>{price}:-</h4>
+                  </div>
+                </div>
+                <div className="my-14 space-y-4">
+                  <h4 className="text-xl font-semibold">
+                    Check out your purchase
+                  </h4>
+                  <div className="flex flex-col gap-4 md:flex-row">
+                    <div className="order-2 flex flex-1 flex-col items-center rounded-xl bg-gray-200 p-8 py-12 text-center">
+                      <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                        <span>Pay Directly</span>
+                        <span>with Swish</span>
+                      </h4>
+                      <Button title="Checkout" type="submit" />
+                      <p className="mt-2 max-w-[300px] text-[12px]">
+                        We do not save any kind of information given to us,
+                        whether it be your address or contact information,
+                        unless it&apos;s valuable information that could
+                        generate us more money in the end
+                      </p>
+                    </div>
+
+                    <div className="flex flex-1 flex-col items-center space-y-8 rounded-xl bg-gray-200 p-8 py-12 text-center md:order-2">
+                      <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                        <span>Pay Monthly</span>
+                        <span>with Klarna</span>
+                      </h4>
+                      <Button
+                        noIcon
+                        title="Check out with Klarna"
+                        type="submit"
+                      />
+                      <p className="mt-2 max-w-[300px] text-[12px]">
+                        We do not save any kind of information given to us,
+                        whether it be your address or contact information,
+                        unless it&apos;s valuable information that could
+                        generate us more money in the end
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </Form>
+      </Formik>
     </main>
   );
 };
