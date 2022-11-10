a3217d9cc6f556034d9d22e108995ec362e65005
Mattias Söderberg <mattes.email@gmail.com>
2022-11-10 14:18:16 +0100

    Add path

--- workspaces/backend/src/utils/initProducts.ts
+++ workspaces/backend/src/utils/initProducts.ts
@@ -2,11 +2,14 @@ import { AppModule } from '../app.module';
 import { NestFactory } from '@nestjs/core';
 import { ProductsService } from '../products/products.service';
 import { readFileSync } from 'fs';
+import * as path from 'path';
 
 export const initProducts = async (): Promise<void> => {
   const app = await NestFactory.createApplicationContext(AppModule);
   const productsService = app.get(ProductsService);
-  const products = JSON.parse(readFileSync('assets/products.json', 'utf8'));
+  const products = JSON.parse(
+    readFileSync(path.resolve(__dirname, '../../assets/products.json'), 'utf8'),
+  );
   const currentProducts = await productsService.findAll();
 
   if (currentProducts.length === 0) {
